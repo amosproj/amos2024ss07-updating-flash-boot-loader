91c20d5122b6545a4481b53a385ad6e2b5df4239
Sebastian Rodriguez <r99@melao.de>
2024-05-05 20:16:01 +0200

    Deleted Unnessary Files, wrote Header File for CAN_Driver, contains defines,includes and header for function that will need to be implemented, init, message trasmition and Interrupt Handler when RX
    Signed-off-by: Sebastian Rodriguez <r99@melao.de>

--- can_driver.h
+++ can_driver.h
@@ -8,3 +8,39 @@
 // Copyright   : MIT
 // Description : Header for CAN Driver
 //============================================================================
+
+#ifndef CAN_DRIVER_H
+#define CAN_DRIVER_H
+
+
+/**********/
+/*INCLUDES*/
+/*********/
+#include <stdio.h>
+#include <string.h>
+#include "Ifx_types.h"
+#include "IfxCan_Can.h"
+#include "IfxCan.h"
+#include "IfxCpu_Irq.h"
+#include "IfxPort.h"
+
+/*********/
+/**MACROS*/
+/*********/
+#define DEBUGGING                   1 //Debug Prints
+#define MAXIMUM_CAN_DATA_PAYLOAD    2 /*8Byte CAN-MESSAGE*/
+
+/*canType struct contains Data Structures needed for config and processing of CAN Messages*/
+typedef struct canType
+{
+    
+}canType;
+
+void initCanDriver(void);
+void transmitCanMessage(uint32 canMessageID, uint32 TXLowDataWord, uint32 TXHighDataWord);
+void canIsrRxHandler(void (*processData)(void*));
+
+
+#endif /*CAN_DRIVER_H*/
+
+
