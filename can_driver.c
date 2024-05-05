7abed07d4ba6412482c646b1a98659a5b416ba60
Sebastian Alberto Rodriguez <128830580+sebas-rodri@users.noreply.github.com>
2024-05-05 18:27:16 +0200

    Renaming

---
+++ can_driver.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Sebastian Rodriguez <r99@melao.de>
+
+//============================================================================
+// Name        : can_driver.c
+// Author      : Sebastian Rodriguez
+// Version     : 0.1
+// Copyright   : MIT
+// Description : C File for CAN Driver
+//============================================================================
