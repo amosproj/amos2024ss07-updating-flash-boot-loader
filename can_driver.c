ae37c1fddee88931fb5437c029c9d23ba2f6a9e2
Sebastian Rodriguez <r99@melao.de>
2024-05-06 13:25:46 +0200

    Implemented init method with src and dest node and module init
    Signed-off-by: Sebastian Rodriguez <r99@melao.de>

--- can_driver.c
+++ can_driver.c
@@ -11,6 +11,10 @@
 
 #include "can_driver.h"
 
+canType g_can //Global control struct
+/*Interrupts*/
+IFX_INTERRUPT(canIsrRxHandler, 0, INTERRUP_PRIO_RX);
+
 /**
  * Interrupt Service Routine (ISR) is called when RX Interrupt is generated
  * Calls function to execute on Data Read in CAN Message
@@ -21,8 +25,41 @@ void canIsrRxHandler(void (*processDataFunction)(void*));
 /**
  * Initialize CAN Module and Node
 */
-void initCanDriver(){
+void initCanDriver(void){
+    IfxCan_Can_initModuleConfig(&g_can.canConfig, &MODULE_CAN0) /*LoadsDefault Config*/
+    IfxCan_Can_initModule(&g_can.canModule, &g_can.canConfig) /*Init with default config*/
 
+
+    //TODO: Change Loopback mode 
+    IfxCan_Can_initNodeConfig(&g_can.canNodeConfig, &g_can.canModule);
+
+    g_can.canNodeConfig.busLoopbackEnabled = TRUE;                               /*Loopback Mode (no external pins)*/
+    g_can.canNodeConfig.nodeId = IfxCan_NodeId_0;                                /*ID = 0*/
+
+    g_can.canNodeConfig.frame.type = IfxCan_FrameType_transmit;                  /*Frame is a transmitting one*/
+
+    g_can.canNodeConfig.interruptConfig.transmissionCompletedEnabled = TRUE;     /*Raises Interrupt when transmition is done*/
+    g_can.canNodeConfig.interruptConfig.traco.priority = ISR_PRIORITY_CAN_TX;    /*Prio*/
+    g_can.canNodeConfig.interruptConfig.traco.interruptLine = IfxCan_InterruptLine_0; /*Interrupt line 0*/
+    g_can.canNodeConfig.interruptConfig.traco.typeOfService = IfxSrc_Tos_cpu0;       /*On CPU0*/
+
+    IfxCan_Can_initNode(&g_can.canSrcNode, &g_can.canNodeConfig);             /*INIT Node with this Config*/
+
+
+    //TODO: Change Loopback mode when PINS are used
+    IfxCan_Can_initNodeConfig(&g_can.canNodeConfig, &g_can.canModule);                  /*Default Config*/
+
+    g_can.canNodeConfig.busLoopbackEnabled = TRUE;                                      /*Loopbackmode*/
+    g_can.canNodeConfig.nodeId = IfxCan_NodeId_1;                                       /*ID = 1*/
+
+    g_can.canNodeConfig.frame.type = IfxCan_FrameType_receive;                          /*Receiving Frame*/
+
+    g_can.canNodeConfig.interruptConfig.messageStoredToDedicatedRxBufferEnabled = TRUE; /*Raise Interrupt when msg is stored in RX Buffer*/
+    g_can.canNodeConfig.interruptConfig.reint.priority = ISR_PRIORITY_CAN_RX;           /*Prio*/
+    g_can.canNodeConfig.interruptConfig.reint.interruptLine = IfxCan_InterruptLine_1;   /*Interrupt Line 1*/
+    g_can.canNodeConfig.interruptConfig.reint.typeOfService = IfxSrc_Tos_cpu0;          /*On CPU 0*/
+
+    IfxCan_Can_initNode(&g_can.canDstNode, &g_can.canNodeConfig);                        /*INIT Node with this Config*/
 }
 
 /**
