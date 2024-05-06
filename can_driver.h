db036d049ddd623573fa1b65cb46d056d3717602
Sebastian Rodriguez <r99@melao.de>
2024-05-06 15:44:20 +0200

    New Header for AURIX specific includes for easier transmition between MCUs
    Signed-off-by: Sebastian Rodriguez <r99@melao.de>

--- can_driver.h
+++ can_driver.h
@@ -18,42 +18,7 @@
 /*********/
 #include <stdio.h>
 #include <string.h>
-#include "Ifx_types.h"
-#include "IfxCan_Can.h"
-#include "IfxCan.h"
-#include "IfxCpu_Irq.h"
-#include "IfxPort.h"
 
-/*********/
-/**MACROS*/
-/*********/
-#define DEBUGGING                   1 //Debug Prints
-#define MAXIMUM_CAN_DATA_PAYLOAD    2 /*8Byte CAN-MESSAGE*/
-#define INTERRUPT_PRIO_RX           1 /*Priority for RX Interrupt*/
-#define INTERRUPT_PRIO_TX           2 /*Prio for TX Interrupt*/
-
-/*canType struct contains Data Structures needed for config and processing of CAN Messages*/
-typedef struct canType
-{
-    IfxCan_Can_Config canConfig;                            /* CAN module configuration structure                   */
-    IfxCan_Can canModule;                                   /* CAN module handle                                    */
-    IfxCan_Can_Node canSrcNode;                             /* CAN source node handle data structure                */
-    IfxCan_Can_Node canDstNode;                             /* CAN destination node handle data structure           */
-    IfxCan_Can_NodeConfig canNodeConfig;                    /* CAN node configuration structure                     */
-    IfxCan_Filter canFilter;                                /* CAN filter configuration structure                   */
-    IfxCan_Message txMsg;                                   /* Transmitted CAN message structure                    */
-    IfxCan_Message rxMsg;                                   /* Received CAN message structure                       */
-    uint32 txData[MAXIMUM_CAN_DATA_PAYLOAD];                /* Transmitted CAN data array                           */
-    uint32 rxData[MAXIMUM_CAN_DATA_PAYLOAD];                /* Received CAN data array                              */
-}canType;
-
-typedef struct msg
-{
-    IfxCan_Message Msg;
-    uint32 Data[MAXIMUM_CAN_DATA_PAYLOAD];
-}msg;
-
-void canInitDriver(void);
 void canTransmitMessage(uint32 canMessageID, uint64_t data, uint64_t len); //oder uint8_t*
 void canIsrRxHandler();
 
