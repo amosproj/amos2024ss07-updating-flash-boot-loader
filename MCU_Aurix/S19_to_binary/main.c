7e3d4091b9e8848d9887d45228505c461de6f02e
wlklsn <leonwilms.wk@gmail.com>
2024-05-06 16:55:27 +0200

    added additional logic to the converter and fixed an error while converting

--- MCU_Aurix/S19_to_binary/main.c
+++ MCU_Aurix/S19_to_binary/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 #include <stdbool.h>
+#include <stdint.h>
 
 /*
 -----------------------------------------------------------------------------------------------------------
@@ -8,7 +9,14 @@
     For this code to work, you will have to link against the "comdlg32" library in the linker settings.
     "GetOpenFileName" will not be found if the linker setup is not correct.
 -----------------------------------------------------------------------------------------------------------
+
+    USED S19 SREC documentation from the wikipedia article here:
+    https://en.wikipedia.org/wiki/SREC_(file_format)
+
+-----------------------------------------------------------------------------------------------------------
 */
+
+
 
 bool convert_s19_to_binary(const char* s19_file) {
     FILE* f_s19 = fopen(s19_file, "r");
@@ -31,10 +39,82 @@ bool convert_s19_to_binary(const char* s19_file) {
 
     char line[256];
     while (fgets(line, sizeof(line), f_s19)) {
-        if (line[0] == 'S' && (line[1] == '1' || line[1] == '2' || line[1] == '3')) {
+
+        if(line[0] =! 'S'){
+
+            fclose(f_s19);
+            fclose(f_bin);
+
+            printf("Corrupt file detected.\n");
+
+            if(remove(f_bin) != 0){
+
+                printf("Error deleting the corrupt bin file.\n");
+                return 1;
+            }
+        }
+
+        if(line[1] == '0'){
+
+            printf("HEADER detected!\n");
+
+            char header_file[MAX_PATH];
+
+
+
+            strncpy(header_file, s19_file, sizeof(header_file) - 4);
+
+            strcat(header_file, "_HEADER.txt");
+
+            printf("HEADER_name: %s \n", header_file);
+
+            FILE* f_header = fopen(header_file, "wb");
+            if (!f_header) {
+                printf("Error: Unable to create binary file.\n");
+                fclose(f_header);
+                return false;
+            }
+            char ascii[2];
+            char* data_start = line + 4;
+            int16_t len;
+
+            sscanf(line + 2, "%2x", &len);
+
+            //len - 1 because we do not want to read the checksum
+            char* data_end = line + 4 + (len * 2) - 2;
+
+            while(data_start < data_end){
+
+                printf("data_start in loop: %d\n", data_start);
+                printf("data_end in loop: %d\n\n", data_end);
+
+                //printf("I: %d\n", len-data_start);
+
+                sscanf(data_start, "%2x", &ascii);
+
+                printf("ascii char: %s\n", ascii);
+
+                fwrite(&ascii, 1, 1, f_header);
+                data_start += 2;
+            }
+
+            fwrite('\0', 1, 1, f_header);
+
+            fclose(f_header);
+
+            printf("HEADER File successful!\n");
+        }
+
+
+        if (line[1] == '1' || line[1] == '2' || line[1] == '3') {
             // Extract data bytes and write them to the binary file
-            char* data_start = line + 2;
-            char* data_end = line + strlen(line) - 3;
+            char* data_start = line + 4;
+            int16_t len;
+
+            sscanf(line + 2, "%2x", &len);
+
+            //len - 1 because we do not want to read the checksum
+            char* data_end = line + 4 + (len * 2) - 2;
             while (data_start < data_end) {
                 int byte;
                 sscanf(data_start, "%2x", &byte);
