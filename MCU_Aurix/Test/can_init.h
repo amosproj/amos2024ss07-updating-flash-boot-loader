36c40e20ab667499e83bf7f139bb4685adba2ac5
wlklsn <leonwilms.wk@gmail.com>
2024-05-07 19:41:49 +0200

    fixed some bugs together with sebastian

---
+++ MCU_Aurix/Test/can_init.h
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Sebastian Rodriguez <r99@melao.de>
+
+//============================================================================
+// Name        : can_driver.h
+// Author      : Sebastian Rodriguez
+// Version     : 0.1
+// Copyright   : MIT
+// Description : Header for CAN Driver with AURIX Includes
+//============================================================================
+
+#include "Ifx_types.h"
+#include "IfxCan_Can.h"
+#include "IfxCan.h"
+#include "IfxCpu_Irq.h"
+#include "IfxPort.h"
+#include <stdint.h>
+
+/*********/
+/**MACROS*/
+/*********/
+#define DEBUGGING                   1 //Debug Prints
+#define MAXIMUM_CAN_DATA_PAYLOAD    1 /*8Byte CAN-MESSAGE*/
+#define INTERRUPT_PRIO_RX           1 /*Priority for RX Interrupt*/
+#define INTERRUPT_PRIO_TX           2 /*Prio for TX Interrupt*/
+
+/*canType struct contains Data Structures needed for config and processing of CAN Messages*/
+typedef struct canType
+{
+    IfxCan_Can_Config canConfig;                            /* CAN module configuration structure                   */
+    IfxCan_Can canModule;                                   /* CAN module handle                                    */
+    IfxCan_Can_Node canSrcNode;                             /* CAN source node handle data structure                */
+    IfxCan_Can_Node canDstNode;                             /* CAN destination node handle data structure           */
+    IfxCan_Can_NodeConfig canNodeConfig;                    /* CAN node configuration structure                     */
+    IfxCan_Filter canFilter;                                /* CAN filter configuration structure                   */
+    IfxCan_Message txMsg;                                   /* Transmitted CAN message structure                    */
+    IfxCan_Message rxMsg;                                   /* Received CAN message structure                       */
+    uint64_t txData[MAXIMUM_CAN_DATA_PAYLOAD];                /* Transmitted CAN data array                           */
+    uint64_t rxData[MAXIMUM_CAN_DATA_PAYLOAD];                /* Received CAN data array                              */
+}canType;
+
+typedef struct msg
+{
+    IfxCan_Message Msg;
+    uint32 Data[MAXIMUM_CAN_DATA_PAYLOAD];
+}msg;
+
+void canInitDriver(void);
---
+++ MCU_Aurix/Test/leds.h
@@ -0,0 +1,7 @@
+#define PIN5                        5                       
+#define PIN6                        6
+
+extern IfxPort_Pin_Config          g_led1;                         /* Global LED1 configuration and control structure      */
+extern IfxPort_Pin_Config          g_led2;
+
+void initLeds(void);
