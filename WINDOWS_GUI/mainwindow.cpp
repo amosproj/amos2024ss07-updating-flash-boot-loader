29b0cb203d1145af5d4b7b2f06d7f92033c71139
Wiktor Pilarczyk <wiktorpilar99@gmail.com>
2024-05-09 22:43:20 +0200

    #27 Renaming
    Signed-off-by: Wiktor Pilarczyk <wiktorpilar99@gmail.com>

--- WINDOWS_GUI/mainwindow.cpp
+++ WINDOWS_GUI/mainwindow.cpp
@@ -21,7 +21,7 @@ MainWindow::MainWindow(QWidget *parent)
     this->setWindowIcon(QIcon::fromTheme("FlashBootloader",
                                          QIcon(qApp->applicationDirPath() + "/../../icon.png")));
 
-    connect(ui->button_choose, &QPushButton::clicked, this, [=]() {
+    connect(ui->button_file, &QPushButton::clicked, this, [=]() {
         QString path = QFileDialog::getOpenFileName(nullptr, "Choose File");
         if(!path.isEmpty()) {
             QFile file(path);
@@ -37,16 +37,16 @@ MainWindow::MainWindow(QWidget *parent)
         }
     });
 
-    ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
-    ui->tableWidget->setSelectionMode(QAbstractItemView::SingleSelection);
-    ui->tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    connect(ui->tableWidget, &QTableWidget::itemSelectionChanged, this, [=]() {
-        QTableWidgetItem *item = ui->tableWidget->selectedItems().at(0);
-            ui->label_2->setText("Selected: " + item->text());
+    ui->table_ECU->setSelectionBehavior(QAbstractItemView::SelectRows);
+    ui->table_ECU->setSelectionMode(QAbstractItemView::SingleSelection);
+    ui->table_ECU->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    connect(ui->table_ECU, &QTableWidget::itemSelectionChanged, this, [=]() {
+        QTableWidgetItem *item = ui->table_ECU->selectedItems().at(0);
+            ui->label_selected_ECU->setText("Selected: " + item->text());
     });
-    connect(ui->pushButton, &QPushButton::clicked, this, [=]() {
-        if(ui->label_2->text() != "")
-            dummy_flash(ui->tableWidget->selectedItems().at(0)->text());
+    connect(ui->button_flash, &QPushButton::clicked, this, [=]() {
+        if(ui->label_selected_ECU->text() != "")
+            dummy_flash(ui->table_ECU->selectedItems().at(0)->text());
     });
 }
 
