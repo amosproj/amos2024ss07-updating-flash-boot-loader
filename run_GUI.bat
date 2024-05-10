c6b264ed59a0d4c3cd8c689dee0268bc556d7a39
Wiktor Pilarczyk <wiktorpilar99@gmail.com>
2024-05-11 01:52:10 +0200

    #33 Automate GUI build process
    Signed-off-by: Wiktor Pilarczyk <wiktorpilar99@gmail.com>

---
+++ run_GUI.bat
@@ -0,0 +1,16 @@
+@echo off
+cd WINDOWS_GUI
+rd /s /q build
+mkdir build
+cd build
+set my_path=%CD%
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release
+call "C:\Qt\6.8.0\msvc2019_64\bin\qtenv2.bat"
+cd %my_path%
+cd Release
+windeployqt.exe --quick .
+call WINDOWS_GUI.exe
+
+REM mklink .\..\..\run_gui.lnk .\WINDOWS_GUI.exe
+
