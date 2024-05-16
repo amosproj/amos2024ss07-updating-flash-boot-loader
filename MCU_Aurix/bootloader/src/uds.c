58e01e1b788aa9d3497e8345080365380a6fc7b9
Dorothea Ehrl <dorothea.ehrl@fau.de>
2024-05-16 17:36:45 +0200

    #43: Add uds file
    Signed-off-by: Dorothea Ehrl <dorothea.ehrl@fau.de>

---
+++ MCU_Aurix/bootloader/src/uds.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 Dorothea Ehrl <dorothea.ehrl@fau.de>
+
+//============================================================================
+// Name        : uds.c
+// Author      : Dorothea Ehrl
+// Version     : 0.1
+// Copyright   : MIT
+// Description : Handles UDS messages from CAN bus
+//============================================================================
+
