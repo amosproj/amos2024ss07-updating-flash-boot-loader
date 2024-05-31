5bcb4cd650ab784cfd4f2c115203bab2728ab2cb
Wiktor Pilarczyk <wiktorpilar99@gmail.com>
2024-05-31 15:40:39 +0200

    #68 Add logo to GUI
    Signed-off-by: Wiktor Pilarczyk <wiktorpilar99@gmail.com>

--- WINDOWS_GUI/mainwindow.cpp
+++ WINDOWS_GUI/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include <QThread>
 #include <QAction>
 #include <QMessageBox>
+#include <QPixmap>
 
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
@@ -96,7 +97,10 @@ MainWindow::MainWindow(QWidget *parent)
     this->setFixedSize(this->geometry().width(),this->geometry().height());
 
     this->setWindowIcon(QIcon::fromTheme("FlashBootloader",
-                                         QIcon("../../icon.png")));
+                                         QIcon("../../images/icon.png")));
+    const QPixmap pix("../../images/logo.png");
+    pix.scaled(100,100, Qt::KeepAspectRatio);
+    ui->label_logo->setPixmap(pix);
 
     qInfo("Main: Create Communication Layer");
     comm = new Communication();
