488c40c17a501f49f02966bcb0dce905977bfcad
Dorothea Ehrl <dorothea.ehrl@fau.de>
2024-06-03 18:14:08 +0200

    #67: Add licenses and Readme to release .zip
    Signed-off-by: Dorothea Ehrl <dorothea.ehrl@fau.de>

--- GUI_Package.bat
+++ GUI_Package.bat
@@ -3,4 +3,4 @@
 
 set dir_path=%CD%
 cd WINDOWS_GUI\build\Release
-tar.exe -caf %dir_path%\GUI.zip *.dll WINDOWS_GUI.exe platforms
+tar.exe -caf %dir_path%\GUI.zip *.dll WINDOWS_GUI.exe platforms -C ..\..\..  README.md LGPL* LICENSE
