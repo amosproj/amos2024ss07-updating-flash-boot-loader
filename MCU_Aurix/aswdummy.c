dc236678bce4a5cd86e37be7087c00dc3d7e20e8
Sebastian Rodriguez <r99@melao.de>
2024-06-10 12:46:36 +0200

    Header and C File for ASW dummy
    Signed-off-by: Sebastian Rodriguez <r99@melao.de>

---
+++ MCU_Aurix/aswdummy.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: 2024 Sebastian Rodriguez <r99@melao.de>
+
+//============================================================================
+// Name        : aswdummy.h
+// Author      : Sebastian Rodriguez
+// Version     : 0.1
+// Copyright   : MIT
+// Description : Implementation file for the dummy ASW
+//============================================================================
+
+#include "aswdummy.h"
