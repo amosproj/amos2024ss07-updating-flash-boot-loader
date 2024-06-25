ca83f9a00665881b217b7610055b308814103d41
Dorothea Ehrl <dorothea.ehrl@fau.de>
2024-06-25 16:01:54 +0200

    #96: Remove build folder of asw from git
    Signed-off-by: Dorothea Ehrl <dorothea.ehrl@fau.de>

--- asw_dummy_new/TriCore Debug (TASKING)/Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.d
+++
@@ -1,8 +0,0 @@
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.c
-../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.c :
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.h
-../Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.h :
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	c:/Users/r99/Desktop/amos2024ss07-updating-flash-boot-loader/asw_dummy_new/Configurations/Ifx_Cfg.h
-c:/Users/r99/Desktop/amos2024ss07-updating-flash-boot-loader/asw_dummy_new/Configurations/Ifx_Cfg.h :
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxScu_cfg.o :	c:/Users/r99/Desktop/amos2024ss07-updating-flash-boot-loader/asw_dummy_new/Libraries/Infra/Sfr/TC37A/_Reg/IfxScu_bf.h
-c:/Users/r99/Desktop/amos2024ss07-updating-flash-boot-loader/asw_dummy_new/Libraries/Infra/Sfr/TC37A/_Reg/IfxScu_bf.h :
--- asw_dummy_new/TriCore Debug (TASKING)/Libraries/iLLD/TC37A/Tricore/_Impl/IfxSent_cfg.d
+++
@@ -1,4 +0,0 @@
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxSent_cfg.o :	../Libraries/iLLD/TC37A/Tricore/_Impl/IfxSent_cfg.c
-../Libraries/iLLD/TC37A/Tricore/_Impl/IfxSent_cfg.c :
-Libraries/iLLD/TC37A/Tricore/_Impl/IfxSent_cfg.o :	../Libraries/iLLD/TC37A/Tricore/_Impl/IfxSent_cfg.h
-../Libraries/iLLD/TC37A/Tricore/_Impl/IfxSent_cfg.h :
