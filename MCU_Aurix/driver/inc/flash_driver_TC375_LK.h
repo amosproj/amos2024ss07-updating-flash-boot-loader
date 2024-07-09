166c7d032cf376ebf564cd9445fd766486ec2631
Michael Bauer <mike.bauer@fau.de>
2024-07-09 23:56:21 +0200

    #131: Alignment of phy to log sectors (MCU)
    Signed-off-by: Michael Bauer <mike.bauer@fau.de>

--- MCU_Aurix/driver/inc/flash_driver_TC375_LK.h
+++ MCU_Aurix/driver/inc/flash_driver_TC375_LK.h
@@ -30,6 +30,12 @@
 #define DATA_FLASH_0                IfxFlash_FlashType_D0
 #define DATA_FLASH_1                IfxFlash_FlashType_D1
 
+/* Important for writing into PFlash and DFlash - Take care about borders for erasing */
+#define PFLASH_SECTOR_LENGTH        0x4000                       /* 16KB - logical sector length */
+#define PFLASH_PHY_SECTOR_LENGTH    0x100000                     /* 1MB - real physical sector length, here reduced to 16KB blocks */
+#define DFLASH_SECTOR_LENGTH        0xFFF                        /* 4KB - default sector length in single ended mode */
+
+
 /* Relocation address for the erase and program routines: Program Scratch-Pad SRAM (PSPR) of CPU0 */
 #define PSPR_START_ADDR             (0x70100000U)
 
@@ -39,12 +45,28 @@
 #define DATA_FLASH_1_BASE_ADDR      0xAFC00000
 #define DATA_FLASH_1_END_ADDR       0xAFC1FFFF
 
-// TODO are these the correct addresses for PF? Depending on segments there are multiple possible addr listed in manual
-#define PROGRAM_FLASH_0_BASE_ADDR   0xA0090000 // TODO eig 0xA0000000
-#define PROGRAM_FLASH_0_END_ADDR    0xA02FFFFF
+// WRITEABLE ADDRESSES - Need to match Physical and Logical Sector sizes and start at a physical sector
+/*
+ * Default PFlash 0:
+ *      Start   0xA0090000
+ *      End     0xA02FFFFF
+ *      Size    0x00270000 -> 2,4375 physical sectors (2 full physical + 28 logical sectors), starting at a physical sector
+ */
+#define PROGRAM_FLASH_0_PHY_BASE_ADDR   0xA0000000  // Real Start Address (used for calculation)
+#define PROGRAM_FLASH_0_PHY_END_ADDR    0xA02FFFFF  // Real End Address
+#define PROGRAM_FLASH_0_BASE_ADDR       0xA0090000  // Writeable Start Address (hard coded boundary to avoid overwriting reserved areas)
+#define PROGRAM_FLASH_0_END_ADDR        0xA02FFFFF  // Writeable End Address (hard coded boundary to avoid overwriting reserved areas)
 
-#define PROGRAM_FLASH_1_BASE_ADDR   0xA0300000
-#define PROGRAM_FLASH_1_END_ADDR    0xA05FFFFF
+/*
+ * Default PFlash 1:
+ *      Start   0xA0300000
+ *      End     0xA05FFFFF
+ *      Size    0x00300000 -> 3 full physical sectors, starting at a physical sector
+ */
+#define PROGRAM_FLASH_1_PHY_BASE_ADDR   0xA0300000  // Real Start Address (used for calculation)
+#define PROGRAM_FLASH_1_PHY_END_ADDR    0xA05FFFFF  // Real End Address
+#define PROGRAM_FLASH_1_BASE_ADDR       0xA0300000  // Writeable Start Address (hard coded boundary to avoid overwriting reserved areas)
+#define PROGRAM_FLASH_1_END_ADDR        0xA05FFFFF  // Writeable End Address (hard coded boundary to avoid overwriting reserved areas)
 
 #define PFLASH_PAGE_LENGTH          IFXFLASH_PFLASH_PAGE_LENGTH /* 0x20 = 32 Bytes (smallest unit that can be
                                                                  * programmed in the Program Flash memory (PFLASH)) */
@@ -52,10 +74,6 @@
 #define DFLASH_PAGE_LENGTH          IFXFLASH_DFLASH_PAGE_LENGTH /* 0x8 = 8 Bytes (smallest unit that can be
                                                                  * programmed in the Data Flash memory (DFLASH))    */
 
-
-#define PFLASH_SECTOR_LENGTH        0x4000                       /* 16KB - logical sector length */
-#define PFLASH_PHY_SECTOR_LENGTH    0x100000                     /* 1MB - real physical sector length, here reduced to 16KB blocks */
-#define DFLASH_SECTOR_LENGTH        0xFFF                        /* 4KB - default sector length in single ended mode */
 /*********************************************************************************************************************/
 /*-------------------------------------------------Global variables--------------------------------------------------*/
 /*********************************************************************************************************************/
